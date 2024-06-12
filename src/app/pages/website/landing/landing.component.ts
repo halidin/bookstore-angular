8f271e4bdbfb018cb9aefc976bccc335e5ad6b5a
Chalidin Karachous <38510056+halidin@users.noreply.github.com>
2024-06-12 14:05:32 +0300

    Update landing.component.ts

--- src/app/pages/website/landing/landing.component.ts
+++ src/app/pages/website/landing/landing.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { Router,RouterModule } from '@angular/router';
-
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { CommonModule, NgFor } from '@angular/common'
 import { TokenHandlerService } from '../../../services/token-handler.service';
 import { AuthService } from '../../../services/auth.service';
 import { ProductService } from '../../../services/product.service'; 
+
 
 
 @Component({
@@ -19,9 +19,7 @@ import { ProductService } from '../../../services/product.service';
 export class LandingComponent {
   cartproductObj: CartProduct;
   newProduct: { productId: string; quantity: number,price:number,title:string,img:string};
-  quan:any
   productList: any [] = [];
-  activeCart!: boolean
   cartId!:string
 
   userCart!: { productId: string; quantity: number,price:number,title:string,img:string}[];
@@ -29,19 +27,21 @@ export class LandingComponent {
   constructor(private productService: ProductService,public authService:AuthService,private tokenHandlerService: TokenHandlerService,private http: HttpClient,private router: Router) {
     this.cartproductObj = new CartProduct();
     this.newProduct = { productId: '', quantity: 1 ,price:0,title:'',img:''};
-    
+
   }
 
   ngOnInit(): void{
-    
     this.getUploadedProducts();
     this.getCart()
   }
-  
-  // Get all products
+
+  // Get all products and add quantity on each product
   getUploadedProducts() {
     this.productService.getUploadedProducts().subscribe({
       next: (res: any) => {
+        res.body.forEach((product: Product) => { 
+          product.quantity = 1; 
+        });
         this.productList = res.body;
       },
       error: (error) => {
@@ -52,21 +52,19 @@ export class LandingComponent {
 
 
   // Add product to cart, check if there is a already saved cart in the database or create new one 
-  addToCart(id: string,price:number,title:string,img:string): void {
+  addToCart(id: string,price:number,title:string,img:string,quantity:number): void {
     const token = this.tokenHandlerService.getToken();
     console.log(token)
     if(token===null
     )
       {alert('Login to order a product')};
+
     const userId = this.authService.getUserId();
     const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
 
-    this.newProduct.productId = id;
+
     this.cartproductObj.userId = userId;
-    this.newProduct.price= price;
-    this.newProduct.title=title
-    this.newProduct.img=img
-    
+    this.newProduct = { productId:id, quantity, price, title, img };
     this.cartproductObj.totalPrice+= this.newProduct.quantity * this.newProduct.price
     
     // Check if there is a cart 
@@ -75,7 +73,7 @@ export class LandingComponent {
       this.cartproductObj.products = [this.newProduct];
       this.http.post('https://bookshop-api-git-main-halidins-projects.vercel.app/api/cart/addtocart', this.cartproductObj, { headers: headers, observe: 'response' })
         .subscribe((res: any) => {
-          alert('Product added to cart');
+          alert('Product added to cart')
           this.getCart(); 
         });
     } else {
@@ -91,7 +89,7 @@ export class LandingComponent {
       
       this.http.post(`https://bookshop-api-git-main-halidins-projects.vercel.app/api/cart/updatecart/${this.cartId}`, this.cartproductObj, { headers: headers, observe: 'response' })
         .subscribe((res: any) => {
-          alert('Product added to cart');
+          alert('Product added to cart')
           this.getCart(); 
         });
     }
@@ -106,7 +104,6 @@ export class LandingComponent {
     this.http.get(`https://bookshop-api-git-main-halidins-projects.vercel.app/api/cart/${userId}`, { headers:headers,observe: 'response' }).subscribe((res: any) => { 
 
       if(res.body.length===0){
-        this.activeCart = false
         this.userCart=[]
       }
       else{this.userCart = res.body[0].products; this.cartId=res.body[0]._id}
@@ -117,6 +114,9 @@ export class LandingComponent {
 }
 
 
+interface Product {
+  productId: string, quantity: number ,price:number,title:string,img:string
+}
 
 export class CartProduct {
   userId: string | null;
